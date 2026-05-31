29a8ba3aeac397b477069be469784c6e4f74f4d5
Veronika050987 <veronicaviv@mail.ru>
2026-05-31 15:40:50 +0300

    ок

---
+++ MySpecialVocabularyForIT/Components/Models/TermTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace MySpecialVocabularyForIT.Components.Models
+{
+	public enum TermTypeEnum
+	{
+		Framework,
+		HtmlMarkup,
+		Technology,
+		Method,
+		ProgrammingLanguage,
+		Other
+	}
+}
